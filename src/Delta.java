14473aa6d72e305b9e6a5393aa6354d8eb200db2
Michał Stadnicki <57849735+michalstadnicki@users.noreply.github.com>
2020-02-05 04:11:04 +0100

    Initial Commit: add switch parameters

--- src/Delta.java
+++ src/Delta.java
@@ -5,10 +5,12 @@ public class Delta {
     public static void main(String[] args) {
 
         double a, b, c, delta, x1, x2;
-        char number_of_elements;
+        int number_of_elements = 0;
+
+        System.out.println("A simple console program that counts the elements of the square equation");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Podaj liczbę a: ");
+        System.out.print("Enter parametr a: ");
         a = scanner.nextDouble();
 
         if (a == 0) {
@@ -18,25 +20,52 @@ public class Delta {
 
         } else {
 
-            System.out.print("Podaj liczbę b: ");
+            System.out.print("Enter parametr b: ");
             b = scanner.nextDouble();
-            System.out.print("Podaj liczbę c: ");
+            System.out.print("Enter parametr c: ");
             c = scanner.nextDouble();
 
-            System.out.println("Funkcja kwadratowa ma postać x = " + a + "+ x^2 " + b + "+ x " + c);
+            System.out.println("Square function x = " + a + "x\u00B2 " + b + "x + " + c);
 
-            delta = (Math.pow(b,2) - (4*a*c));
+            delta = (Math.pow(b, 2) - (4 * a * c));
 
-            System.out.println(delta);
+            System.out.println("\u0394 = " + delta);
 
-            x1 = ((-b-Math.sqrt(delta))/2*a);
-            x2 = ((-b+Math.sqrt(delta))/2*a);
+            if (delta < 0) number_of_elements = 0;
+            if (delta == 0) number_of_elements = 1;
+            if (delta > 0) number_of_elements = 2;
 
-            System.out.println(x1);
-            System.out.println(x2);
+            switch (number_of_elements) {
+                case 0: {
+
+                    System.out.println("The equation has no square root");
+                    break;
+                }
+
+                case 1: {
+
+                    System.out.println("The equation has one square root");
+                    break;
+                }
+
+                case 2: {
+
+                    System.out.println("The equation has two square roots");
+
+                    x1 = ((-b - Math.sqrt(delta)) / 2 * a);
+                    x2 = ((-b + Math.sqrt(delta)) / 2 * a);
+
+                    System.out.printf("%4.2f", x1);
+                    System.out.println("");
+                    System.out.printf("%4.2f", x2);
+                    break;
+                }
+            }
+            
         }
 
         scanner.close();
 
     }
+
 }
